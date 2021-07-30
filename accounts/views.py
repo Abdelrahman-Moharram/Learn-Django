4e615b859398b148f6176a5d9bf58558cdfeaec9
Abdelrahman-Moharram <abd0m07arram@yahoo.com>
2021-07-30 04:56:52 +0200

    add some details in accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def user_login(request):
+    pass
+def user_register(request):
+    pass
+def user_logout(request):
+    pass
+def profile(request):
+    pass
