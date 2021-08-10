4916c3a07e9cd2f381f526866915ac9f1ee85efe
Abdelrahman-Moharram <abd0m07arram@yahoo.com>
2021-08-10 03:28:27 +0200

    fix some bugs

--- project/settings.py
+++ project/settings.py
@@ -143,7 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'abdumo7aram@gmail.com'
-EMAIL_HOST_PASSWORD = 'bzcgmstcsxqtwfpe'
+EMAIL_HOST_USER = 'YOUR EMAIL HERE'
+EMAIL_HOST_PASSWORD = 'YOUR GOOGLE PATH HERE'
 EMAIL_USE_TLS = True
-EMAIL_PORT = '587'
+EMAIL_PORT = ''
